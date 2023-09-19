48acfece3abba7e3e9482de22f6e41d4aea0aa90
YilingChen <shutuzi88@gmail.com>
2023-09-19 14:49:27 +0800

    ai revised code

--- ai.momo.py
+++ ai.momo.py
@@ -1,4 +1,3 @@
-# -*- coding:utf-8 -*-
 import os
 import time
 import random
@@ -12,34 +11,33 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 
-# Momo URL
+# Constants
 MOMO_URL = 'https://www.momoshop.com.tw/main/Main.jsp'
-
-# User agent
-USER_AGENT = ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 '
-            '(KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36')
+USER_AGENT = (
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 '
+    '(KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'
+)
 HEADERS = {'User-Agent': USER_AGENT, 'Referer': 'https://www.momoshop.com.tw'}
 
-# Set up the Chrome driver
+# Global variables
 driver = webdriver.Chrome()
 
 
-def generate_url(keyword, page_no):
+def generate_search_url(keyword, page_no):
     return f'https://www.momoshop.com.tw/search/searchShop.jsp?keyword={urllib.parse.quote(keyword)}&searchType=6&curPage={page_no}&_isFuzzy=0&showType=chessboardType'
 
 
 def open_chrome_browser(keyword, page_no):
-    url = generate_url(keyword, page_no)
+    url = generate_search_url(keyword, page_no)
     driver.get(url)
-    driver.implicitly_wait(10)
+    driver.implicitly_wait(random.randrange(10))
     time.sleep(random.randrange(10))
-    driver.implicitly_wait(30)
+    driver.implicitly_wait(random.randrange(10))
 
 
-def mimic_human():
+def mimic_human_scroll():
     try:
         html = driver.find_element(By.TAG_NAME, 'html')
-        # Simulate human interaction with the page
         for _ in range(6):
             html.send_keys(Keys.END)
             time.sleep(random.randrange(10))
@@ -47,37 +45,45 @@ def mimic_human():
             time.sleep(random.randrange(10))
         print("Finally finished!")
     except TimeoutException:
-        print("Timed out waiting for page to load")
+        print("Timed out waiting for the page to load")
     finally:
         print("Page loaded")
 
 
-def get_last_page_no():
-    last_page = driver.find_element(By.CSS_SELECTOR, '.pageArea > ul > li:last-child > a')
-    last_page_no = int(last_page.get_attribute('pageidx'))
-    print(last_page_no)
+def get_last_page_number():
+    last_page_elem = driver.find_element(
+        By.CSS_SELECTOR, '.pageArea > ul > li:last-child > a')
+    last_page_no = int(last_page_elem.get_attribute('pageidx'))
+    print(f'Last page number: {last_page_no}')
     return last_page_no
 
 
-def give_a_break(n):
-    time.sleep(random.randrange(n))
+def wait_for_a_while(n):
+    time.sleep(n)
 
 
-def download_page(element, n):
+def download_page_to_file(element, n, keyword_folder):
+    if not os.path.exists(keyword_folder):
+        os.makedirs(keyword_folder)
+
     if element.is_displayed():
-        with open(f'momo{n}.html', 'w') as f:
+        with open(os.path.join(keyword_folder, f'momo{n}.html'), 'w') as f:
             f.write(driver.page_source)
     else:
-        print('element display none')
+        print('Element is not displayed')
 
 
-def df_to_excel(data_dict):
+def df_to_excel(data_dict, keyword_folder):
+    if not os.path.exists(keyword_folder):
+        os.makedirs(keyword_folder)
+
+    file_path = os.path.join(keyword_folder, 'output.xlsx')
     df = pd.DataFrame(data_dict)
-    df[['prdName', 'price', 'goodsUrl', 'prdImg']].to_excel('output.xlsx')
+    df[['prdName', 'price', 'goodsUrl', 'prdImg']].to_excel(file_path, index=False)
 
 
-def write_to_dict(n, data_dict):
-    file_path = f'momo{n}.html'
+def parse_html_to_dict(n, data_dict, keyword_folder):
+    file_path = os.path.join(keyword_folder, f'momo{n}.html')
     if os.path.exists(file_path):
         with open(file_path, 'r') as f:
             contents = f.read()
@@ -86,27 +92,33 @@ def write_to_dict(n, data_dict):
             for good in goods:
                 data_dict["prdName"].append(good.select_one('.prdName').text)
                 data_dict["price"].append(good.select_one('.price > b').text)
-                data_dict["goodsUrl"].append('https://www.momoshop.com.tw/' + good.select_one('.goodsUrl')['href'])
+                data_dict["goodsUrl"].append(
+                    'https://www.momoshop.com.tw/' + good.select_one('.goodsUrl')['href'])
                 data_dict["prdImg"].append(good.select_one('.prdImg')['src'])
     else:
-        print('File not exists')
+        print('File does not exist')
 
 
+KEYWORD = '即期品'
 if __name__ == '__main__':
-    keyword = '即期品'
-    open_chrome_browser(keyword, 1)
-    last_page_no = get_last_page_no()
+    if not KEYWORD:
+        KEYWORD = input("Please type the keyword: ")
+
+    open_chrome_browser(KEYWORD, 1)
+    last_page_no = get_last_page_number()
     data_dict = {"prdName": [], "price": [], "goodsUrl": [], "prdImg": []}
+    # Replace spaces with underscores for the folder name
+    keyword_folder = KEYWORD.replace(" ", "_")
 
     for i in range(1, last_page_no + 1):
-        open_chrome_browser(keyword, i)
-        mimic_human()
+        open_chrome_browser(KEYWORD, i)
+        # mimic_human_scroll()
         element = driver.find_element(By.CSS_SELECTOR, '.listArea > ul > li')
         print('Next page')
-        download_page(element, i)
-        write_to_dict(i, data_dict)
-        # give_a_break(random.randrange(10))
+        download_page_to_file(element, i, keyword_folder)
+        parse_html_to_dict(i, data_dict, keyword_folder)
+        wait_for_a_while(random.randrange(10))
 
-    df_to_excel(data_dict)
+    df_to_excel(data_dict, keyword_folder)
     print('Finally finished')
     driver.quit()
